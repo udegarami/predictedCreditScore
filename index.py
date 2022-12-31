4c1bb87e29043445847de92d3986e4af52c878ef
Marco Giangreco <giangreco.marco@gmx.ch>
2022-12-31 14:07:27 +0100

    First commit

---
+++ index.py
@@ -0,0 +1,19 @@
+import streamlit as st
+header = st.container()
+dataset = st.container()
+features = st.container()
+modelTraining = st.container()
+
+with header: 
+    st.title('Loan Payback Estimator')
+
+with dataset: 
+    st.header('Banking Data')
+    st.text('Find the Dataset @ URL: ')
+
+with features: 
+    st.header('Features')
+
+with modelTraining: 
+    st.header('Test')
+    st.text('The Hyperparameters are')
