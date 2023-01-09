7206c8ff0fc2e9933ff052934474c4ebd9ff389f
Marco Giangreco <giangreco.marco@gmx.ch>
2023-01-09 14:32:49 +0100

    Hello Marco

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ with open('xgboost_calibrated.csv', newline='') as csv_file:
 @app.get("/")
 async def root():
     #await request()
-    return {"Hello": "World"}
+    return {"Hello": "Marco"}
 
 @app.get("/api/v1/users")
 async def fetch_users():
